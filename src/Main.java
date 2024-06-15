de2d259af792d10ae0176ae7ccca0b35d8084591
Андрей <najd1703@mailm.ru>
2024-06-15 14:30:25 +0300

    12

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int age = 18;
+        if (age == 18) {
+            System.out.println("С окончанием школы");
+        } else if (age ==21) {
+            System.out.println("Теперь тебе можно пить алко");
+        } else if (age ==7) {
+            System.out.println("иди в школу");
+        } else {
+            System.out.println("Пока сказать нечего");
+
+
+
+    }
+        }
+    }
